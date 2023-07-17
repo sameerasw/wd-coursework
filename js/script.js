3acc3b8cc0d7d1d472bbd6534557ef4eaefc44f0
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-07-17 15:29:23 +0530

    Added sitewide accent color

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 let theme_selector = document.getElementById("theme-selector"); //get the selector element from html to detect theme changes
+let theme = localStorage.getItem("theme");
+
 
 // Path: js\script.js
 theme_selector.addEventListener("change", function () {
     let theme = theme_selector.value;
+    if (theme == "cyan") {
+        localStorage.setItem("theme","cyan");
+    } else if (theme == "crimson") {
+        localStorage.setItem("theme","crimson");
+    } else if (theme == "butter") {
+        localStorage.setItem("theme","butter");
+    }
+    change_theme(theme);
+});
+
+change_theme = (theme) => {
     if (theme == "cyan") {
         document.getElementById("theme").setAttribute("href", "css/theme-cyan.css");
     } else if (theme == "crimson") {
@@ -10,4 +23,7 @@ theme_selector.addEventListener("change", function () {
     } else if (theme == "butter") {
         document.getElementById("theme").setAttribute("href", "css/theme-butter.css");
     }
-});
+}
+
+
+change_theme(theme); //change theme on page load
