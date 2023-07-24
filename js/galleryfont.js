b0b646542f32ae425dad59a8b07d438ef3e44154
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-07-24 14:04:29 +0530

    fix for gallery videos by chathura

---
+++ js/galleryfont.js
@@ -0,0 +1,9 @@
+function fontsize(value) {
+    document.getElementById("view").style.fontSize = value;
+    document.getElementById("view1").style.fontSize = value;
+    document.getElementById("view2").style.fontSize = value;
+    document.getElementById("view3").style.fontSize = value;
+    document.getElementById("view4").style.fontSize = value;
+    document.getElementById("topic1").style.fontSize = value;
+    document.getElementById("topic2").style.fontSize = value;
+  }
