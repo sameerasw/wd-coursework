cb2cb052fdd365351500b251db11ba51c5842c10
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-07-26 22:28:05 +0530

    some missed changes before submission

--- js/gallery.js
+++ js/gallery.js
@@ -23,13 +23,6 @@ close.querySelector('.fa-times').onclick = () =>{
 });
 
 
-const colorTheme=(color,font)=>{
-  document.getElementById('body').className=color+'-background '+font+"-font";
-}
-
-const fontSize = (font,color)=>{
-  document.getElementById('body').className = font+"-font "+color+"-background"
-}
 
 
 
